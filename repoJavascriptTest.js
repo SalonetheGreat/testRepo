b50ee9bb1f6d371caf8c38e0de312332c9c93a95
Leon Sha <apple20200108@appledeMacBook-Pro.local>
2020-06-07 10:22:40 +0800

    add a.out to gitignore

--- repoJavascriptTest.js
+++ repoJavascriptTest.js
@@ -1,2 +1,11 @@
-var a = '';
-console.log(Boolean(a));
+function addThemAll() {
+    var count = 0;
+    for (var i = 0; i < arguments.length; ++i) {
+        count += arguments[i];
+    }
+    return count;
+}
+
+var a = addThemAll(1, 2, 3, 4);
+
+console.log(a);
