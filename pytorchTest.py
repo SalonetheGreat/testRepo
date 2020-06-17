b3fb189f47a1de8ba9f8f65bc4d24fe7b6138ff9
Leon Sha <apple20200108@appledeMacBook-Pro.local>
2020-06-17 15:38:23 +0800

    pytorchtest

---
+++ pytorchTest.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+import torch
+x = torch.zeros(5, 3, dtype=torch.long)
+
+
+print (x)
--- repoJavascriptTest.js
+++
@@ -1,7 +0,0 @@
-var array = [1, 2, 3, 4];
-array.length++;
-console.log(array);
-
-array.reverse();
-
-console.log(array);
