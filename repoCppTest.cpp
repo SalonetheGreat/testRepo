440c2e08c28587475f254681a0b838282219ab32
Sha Long <shalong5794@gmail.com>
2020-06-25 13:25:33 +0800

    add pythonTest

--- repoCppTest.cpp
+++ repoCppTest.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cstdint>
+#include <climits>
+#include <vector>
+#include <algorithm>
 
-int main(){
-    std::cout << "nice to meet you\n";
-    std::cout << "first push\n";
-    std::cout << "second commit\n";
-    return 0;
-}
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+    	int start = 0, end = nums.size()-1, middle;
+    	if (nums.empty())
+    		return 0;
+    	if (nums.size() == 1) {
+    		if (nums[0] < target)
+    			return 1;
+    		return 0;
+    	}
+
+    	// >2 elements
+    	while (1) {
+    		middle = (start+end)/2;
+    		if (nums[middle] == target)
+    			return middle;
+    		if (end-start == 1) {
+    			if (nums[start] > target)
+    				return start;
+    			if (nums[end] < target)
+    				return end+1;
+    			return end; 
+    		}
+
+    		if (nums[middle] < target) {
+    			// target should on RHS (may include middle)
+    			start = middle;
+    		}
+    		else {
+    			// target should on LHS (may include middle)
+    			end = middle;
+    		}
+    	}
+    }
+};
---
+++ repoJavascriptTest.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var max_len = 0;
+    var umap = new Map();
+    var i, j;
+    for (i = 0, j = 0; j < s.length; j++) {
+    	if (umap)
+    }
+};
+
+var s = 'abcabcbb'
+console.log(lengthOfLongestSubstring(s));
