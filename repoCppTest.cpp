12630cf5b48fd57a7c37a743982de9e3ca44e2da
SalonetheGreat <shalong5794@gmail.com>
2020-07-03 12:33:48 +0800

    add cog

--- repoCppTest.cpp
+++ repoCppTest.cpp
@@ -5,42 +5,23 @@
 #include <climits>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-    	int start = 0, end = nums.size()-1, middle;
-    	if (nums.empty())
-    		return 0;
-    	if (nums.size() == 1) {
-    		if (nums[0] < target)
-    			return 1;
-    		return 0;
-    	}
 
-    	// >2 elements
-    	while (1) {
-    		middle = (start+end)/2;
-    		if (nums[middle] == target)
-    			return middle;
-    		if (end-start == 1) {
-    			if (nums[start] > target)
-    				return start;
-    			if (nums[end] < target)
-    				return end+1;
-    			return end; 
-    		}
-
-    		if (nums[middle] < target) {
-    			// target should on RHS (may include middle)
-    			start = middle;
-    		}
-    		else {
-    			// target should on LHS (may include middle)
-    			end = middle;
-    		}
-    	}
-    }
-};
+int main(int argc, char const *argv[])
+{
+/*[[[cog
+import cog
+fnames = ['DoSomething', 'DoAnotherThing', 'DoLastThing']
+for fn in fnames:
+    cog.outl("void %s();" % fn)
+]]]*/
+void DoSomething();
+void DoAnotherThing();
+void DoLastThing();
+//[[[end]]]
+    printf("nice");
+    return 0;
+}
